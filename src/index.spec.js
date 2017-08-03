2daae2e82ca35a245848afced12735b3cd2e360d
Hassan <hfalahi@gmail.com>
2017-08-03 13:07:54 +0430

    fix some bugs in test file

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,4 +2,8 @@ var index = require('./index.js');
 
 test('Nationalcode must be valid', ()=>{
     expect(index.isValid('1111111111')).toBe(true);
-})
+});
+
+test('Nationalcode must not be valid', ()=>{
+    expect(index.isValid('7512698452')).toBe(false);
+});
